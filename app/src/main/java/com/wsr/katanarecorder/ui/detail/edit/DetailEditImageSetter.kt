4e576eb342081aee14ca9e591ada6bdb8408c631
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2021-01-21 15:28:28 +0900

    [fix] 画像の選択と撮影の選択肢を設定

--- app/src/main/java/com/wsr/katanarecorder/ui/detail/edit/DetailEditImageSetter.kt
+++ app/src/main/java/com/wsr/katanarecorder/ui/detail/edit/DetailEditImageSetter.kt
@@ -1,5 +1,6 @@
 package com.wsr.katanarecorder.ui.detail.edit
 
+import android.app.AlertDialog
 import android.net.Uri
 import android.os.Environment
 import android.util.Log
@@ -32,41 +33,56 @@ class DetailEditImageSetter(
         //SAFの呼び出しや、選択後の挙動を設定
         getContent =
             registry.register(
-                "key",
+                "select-key",
                 owner,
-                ActivityResultContracts.GetContent()){
-                it?.let{
+                ActivityResultContracts.GetContent()
+            ){
+                it?.let {
                     resetController(it)
                 }
             }
 
+        //画像の撮影や、その後の挙動を設定
         dispatchTakePicture =
             registry.register(
-                "keys",
+                "take-keys",
                 owner,
-                ActivityResultContracts.TakePicture()){
-                    Log.d("TEST", it.toString())
+                ActivityResultContracts.TakePicture()
+            ){
+                if (it) {
+                    Log.d("takePicture", "Success")
                     resetController(uri)
+                } else {
+                    Log.d("takePicture", "Failed")
                 }
+            }
     }
 
     //画像選択をするための関数
     fun selectImage() {
-        //getContent.launch("image/*")
+        val items = arrayOf("画像を選択", "撮影する")
 
+        AlertDialog.Builder(activity)
+            .setItems(items) { dialog, which ->
+                Log.d("dialog", dialog.toString())
+                Log.d("which", which.toString())
+                when(which){
+                    0 -> getContent.launch("image/*")
+                    1 -> takePicture()
+                }
+            }
+            .show()
+    }
+
+    private fun takePicture() {
         val filename = UUID.randomUUID().toString() + ".jpg"
         val path = activity.getExternalFilesDir(Environment.DIRECTORY_PICTURES)
         val file = File(path, filename)
 
         uri = FileProvider.getUriForFile(activity, BuildConfig.APPLICATION_ID + ".provider", file)
 
-        takePicture()
-    }
-
-    private fun takePicture() {
         dispatchTakePicture.launch(uri)
         Log.d("Uri", uri.toString())
-
     }
 
     //editViewModelに保管されている画像のinputStream型を取るための関数
